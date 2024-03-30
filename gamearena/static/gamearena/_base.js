a3e02484d119aaee5632beef6a20888dda35e222
Gabriel Akara <kaielakara@gmail.com>
2024-03-30 12:21:29 +0100

    Nav Bar implementation
    Implementation of Responsive Nav Bar with just Html, Css and JS

--- gamearena/static/gamearena/_base.js
+++ gamearena/static/gamearena/_base.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
 
+const menu = document.querySelector(".menu");
+const nav_list = document.querySelector(".nav__list");
+const nav_trans = document.querySelector(".nav__trans")
+
+menu.addEventListener("click", () => {
+    const aria = menu.getAttribute("aria-expanded")
+    if (aria === "true") {
+        nav_list.setAttribute("data-visible", true);
+        nav_trans.classList.add("nav__trans--active");
+    } else {
+        nav_list.setAttribute("data-visible", false)
+        nav_trans.classList.remove("nav__trans--active");
+    }
 })
+
+
